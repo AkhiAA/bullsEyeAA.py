fb092852ee3085f8d61b413ab7b4e5cecc0f4c85
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-11 15:01:52 -0400

    Added multiple circles with for loop

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -8,11 +8,15 @@ def draw_cir(cX, cY, size, color, bullWin):
 
 cirSz=50
 cirCol = "dark red"
+cirX = cirSz*5
+cirY = cirSz*5
 
 bullWin = GraphWin("BullsEye",cirSz*10, cirSz*10)
 bullWin.setCoords(0,0,cirSz*10, cirSz*10)
 
-draw_cir(cirSz*5, cirSz*5, cirSz, cirCol, bullWin)
+for i in range(4):
+    cirSz -= 10
+    draw_cir(cirX, cirY, cirSz, cirCol, bullWin)
 
 
 

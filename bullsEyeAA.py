355d4a0d5e67ae780c1ada3d1c2602ef387d250f
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-11 10:56:13 -0400

    got red circle in the center of the window

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -1,20 +1,21 @@
 # bullsEye by akhi abdullah
 from graphics import *
 
-#def draw_cir(Cx, Cy, size, color, win):
- #   circle = circle(point(Cx,Cy),point(Cx, Cy))
- #   circle .setFill(color)
- #   circle. draw(win)
+#def draw_cir(Cx, Cy, size, color, bullWin):
+#    circle = circle(point(Cx,Cy),point(Cx, Cy))
+#    circle.setFill(color)
+#    circle.draw(bullWin)
 
 cirSz=50
+cirCol = "red"
 
 bullWin = GraphWin("BullsEye",cirSz*10, cirSz*10)
-bullWin.setCoords(0,0,cirSz*10, cirSz*10)
+bullWin.setCoords(0,0,900, 900)
 
 
-
-
-
+rCircle = Circle(Point(450,450), 25.5)
+rCircle.setFill(color_rgb(230,10,10))
+rCircle.draw(bullWin)
 
 
 

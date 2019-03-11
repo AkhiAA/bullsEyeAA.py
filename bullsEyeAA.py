bf4b66c428e6b65194ff5d652a54eaa1590fa130
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-11 10:40:05 -0400

    got window with name bullsEye

---
+++ bullsEyeAA.py
@@ -0,0 +1,28 @@
+# bullsEye by akhi abdullah
+from graphics import *
+
+#def draw_cir(Cx, Cy, size, color, win):
+ #   circle = circle(point(Cx,Cy),point(Cx, Cy))
+ #   circle .setFill(color)
+ #   circle. draw(win)
+
+cirSz=50
+
+bullWin = GraphWin("BullsEye",cirSz*10, cirSz*10)
+bullWin.setCoords(0,0,cirSz*10, cirSz*10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bullWin.getMouse()
+bullWin.close()

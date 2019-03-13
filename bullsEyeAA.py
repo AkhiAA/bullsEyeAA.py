5b7dcab77c63de5c6defd21b2d43966158ba2af8
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-13 09:16:49 -0400

    added alternating colors within bullsEye

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -1,10 +1,14 @@
 # bullsEye by akhi abdullah
+
 from graphics import *
 
 def draw_cir(cX, cY, size, color, bullWin):
     circle = Circle(Point(cX,cY), size)
     circle.setFill(color)
     circle.draw(bullWin)
+
+def draw_be(bX, bY, bSize, bColor, bullWin):
+    
 
 cirSz=50
 cirCol = "dark red"
@@ -15,9 +19,13 @@ bullWin = GraphWin("BullsEye",cirSz*10, cirSz*10)
 bullWin.setCoords(0,0,cirSz*10, cirSz*10)
 
 for i in range(4):
+    if (i) % 2 == 1:
+        cirCol = "white"
+    else:
+        cirCol = "red"
+    
     cirSz -= 10
     draw_cir(cirX, cirY, cirSz, cirCol, bullWin)
-
 
 
 

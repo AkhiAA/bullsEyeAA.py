8eff51bfe81147d8e13c05a2a2a89a719393f13d
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-13 12:43:26 -0400

    Added variable so you can change the color of the bullsEye by calling the bullsEye function

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -2,44 +2,34 @@
 
 from graphics import *
 
-cirSz=50
-cirCol = "red"
-cirX = cirSz*5
-cirY = cirSz*5
-
-
 def draw_cir(cX, cY, size, color, bullWin):
     circle = Circle(Point(cX,cY), size)
     circle.setFill(color)
     circle.draw(bullWin)
 
-def draw_be(bX, bY, rings, bSize, bColor, bullWin):
+def draw_be(bX, bY, rings, bSize, bColor1, bColor2, bullWin):
     for i in range (rings):
         if (i) % 2 == 1:
-            bColor = "white"
+            bColor = bColor1
         else:
-            bColor = "red"
+            bColor = bColor2
 
         bSize -= 10
         draw_cir(bX, bY, bSize, bColor, bullWin)
         
+cirSz=60
+cirCol = "red"
+cirX = cirSz*5
+cirY = cirSz*5
     
-    
-
-
+   
 bullWin = GraphWin("BullsEye",cirSz*10, cirSz*10)
 bullWin.setCoords(0,0,cirSz*10, cirSz*10)
 
-##for i in range(4):
-##    if (i) % 2 == 1:
-##        cirCol = "white"
-##    else:
-##        cirCol = "red"
-##    
-##    cirSz -= 10
+
 ##    draw_cir(cirX, cirY, cirSz, cirCol, bullWin)
 
-draw_be(cirX, cirY, 6, cirSz, cirCol, bullWin)
+draw_be(cirX, cirY, 6, cirSz, cirCol, "blue", bullWin)
 
 
 bullWin.getMouse()

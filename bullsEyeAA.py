4c4e7e29a9421aab93984e771d83a35da30052ba
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-13 09:34:00 -0400

    Made custom function for bullsEye with one color and four rings

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -8,6 +8,11 @@ def draw_cir(cX, cY, size, color, bullWin):
     circle.draw(bullWin)
 
 def draw_be(bX, bY, bSize, bColor, bullWin):
+    for i in range (4):
+        bSize -= 10
+        draw_cir(bX, bY,bSize, bColor, bullWin)
+        
+    
     
 
 cirSz=50
@@ -18,16 +23,16 @@ cirY = cirSz*5
 bullWin = GraphWin("BullsEye",cirSz*10, cirSz*10)
 bullWin.setCoords(0,0,cirSz*10, cirSz*10)
 
-for i in range(4):
-    if (i) % 2 == 1:
-        cirCol = "white"
-    else:
-        cirCol = "red"
-    
-    cirSz -= 10
-    draw_cir(cirX, cirY, cirSz, cirCol, bullWin)
+##for i in range(4):
+##    if (i) % 2 == 1:
+##        cirCol = "white"
+##    else:
+##        cirCol = "red"
+##    
+##    cirSz -= 10
+##    draw_cir(cirX, cirY, cirSz, cirCol, bullWin)
 
-
+draw_be(cirX, cirY, cirSz, cirCol, bullWin)
 
 bullWin.getMouse()
 bullWin.close()

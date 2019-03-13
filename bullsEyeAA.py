464eefba4a90bf241ef3d68453241e2cfa7d92cc
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-13 09:46:09 -0400

    Added for loop for any number of rings in bullsEye

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -7,16 +7,17 @@ def draw_cir(cX, cY, size, color, bullWin):
     circle.setFill(color)
     circle.draw(bullWin)
 
-def draw_be(bX, bY, bSize, bColor, bullWin):
-    for i in range (4):
+def draw_be(bX, bY, rings, bSize, bColor, bullWin):
+    for i in range (rings):
+        bSize = rings * 10
         bSize -= 10
-        draw_cir(bX, bY,bSize, bColor, bullWin)
+        draw_cir(bX, bY, bSize, bColor, bullWin)
         
     
     
 
 cirSz=50
-cirCol = "dark red"
+cirCol = "red"
 cirX = cirSz*5
 cirY = cirSz*5
 
@@ -32,7 +33,8 @@ bullWin.setCoords(0,0,cirSz*10, cirSz*10)
 ##    cirSz -= 10
 ##    draw_cir(cirX, cirY, cirSz, cirCol, bullWin)
 
-draw_be(cirX, cirY, cirSz, cirCol, bullWin)
+draw_be(cirX, cirY, 6, cirSz, cirCol, bullWin)
+
 
 bullWin.getMouse()
 bullWin.close()

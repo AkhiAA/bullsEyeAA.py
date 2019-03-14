a23f1e9d79330b6b69d7bffc21b2ffcd6e1ad427
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-14 17:15:24 -0400

    Got bullsEye to be populate with different colors

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -19,8 +19,8 @@ def draw_be(bX, bY, rings, rSize, bColor1, bColor2, bullWin):
         bSize -= rSize
         
 cirSz=60
-cirCol1 = "red"
-cirCol2 = "yellow"
+#cirCol1 = "red"
+cirCol2 = "orange"
 cirX = cirSz*5
 cirY = cirSz*5
     
@@ -32,6 +32,10 @@ for j in range (20):
     ranX = randint(cirSz, cirSz * 9)
     ranY = randint(cirSz, cirSz * 9)
     ranRs = randint(3, 20)
+    ranRed = randint(0,255)
+    ranG = randint(0,255)
+    ranB = randint(0,255)
+    cirCol1 = color_rgb(ranRed, ranG, ranB)
     draw_be(ranX, ranY, ranRs, 6, cirCol1, cirCol2, bullWin)
 
 

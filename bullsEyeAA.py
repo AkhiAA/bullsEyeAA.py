75ce9969a89648f22ddddd796d4a3948e6571d70
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-14 15:59:54 -0400

    made bSize = rSize so that i can make multiple circles with different radius

--- bullsEyeAA.py
+++ bullsEyeAA.py
@@ -7,18 +7,19 @@ def draw_cir(cX, cY, size, color, bullWin):
     circle.setFill(color)
     circle.draw(bullWin)
 
-def draw_be(bX, bY, rings, bSize, bColor1, bColor2, bullWin):
+def draw_be(bX, bY, rings, rSize, bColor1, bColor2, bullWin):
+    bSize = rSize * rings
     for i in range (rings):
         if (i) % 2 == 1:
             bColor = bColor1
         else:
             bColor = bColor2
-
-        bSize -= 10
         draw_cir(bX, bY, bSize, bColor, bullWin)
+        bSize -= rSize
         
 cirSz=60
-cirCol = "red"
+cirCol1 = "red"
+cirCol2 = "yellow"
 cirX = cirSz*5
 cirY = cirSz*5
     
@@ -27,9 +28,7 @@ bullWin = GraphWin("BullsEye",cirSz*10, cirSz*10)
 bullWin.setCoords(0,0,cirSz*10, cirSz*10)
 
 
-##    draw_cir(cirX, cirY, cirSz, cirCol, bullWin)
-
-draw_be(cirX, cirY, 6, cirSz, cirCol, "blue", bullWin)
+draw_be(cirX, cirY, 100, 3, cirCol1, cirCol2, bullWin)
 
 
 bullWin.getMouse()
